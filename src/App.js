d0bed25592b20a8ecbeba3ef23110c47829d9cb2
Chris Kim <ckjr92@gmail.com>
2022-01-22 23:24:21 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+	const [expense, setExpense] = useState([]);
+	const [totalExpense, setTotalExpense] = useState(0);
+
+	useEffect(() => {
+		let temp = 0;
+		for(let i = 0; i < expense.length; i++) {
+			temp += parseInt(expense[i].price);
+		}
+
+		setTotalExpense(temp);
+	}, [expense]);
+	
+
+	return (
+		<div className="App">
+			<Header totalExpense={totalExpense} />
+			<ExpenseForm expense={expense} setExpense={setExpense} />
+			<ExpenseList expense={expense} setExpense={setExpense} />
+		</div>
+	);
 }
 
 export default App;
